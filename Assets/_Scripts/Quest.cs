f666502211bb809fa9c90ce712a4ca6b01419f48
Wiktor Lesiak <B00087701@student.itb.ie>
2018-10-25 16:47:48 +0100

    added font and quest

---
+++ Assets/_Scripts/Quest.cs
@@ -0,0 +1,18 @@
+using System;
+
+public class Quest {
+
+    public int id;
+    public string name;
+    public string description;
+
+
+    public Quest (int id, string name, string description)
+    {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+    }
+
+
+}
